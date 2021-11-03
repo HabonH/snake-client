1c6b8a2217a06fed2df939027b49a398f7de5d3b
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2021-11-03 14:37:27 -0400

    Snake Game w/ module.exports

--- play.js
+++ play.js
@@ -1,25 +1,9 @@
 // IP: 165.227.47.243 PORT: 50541 -- tells clients where to make the connection 
-const net = require("net");
+// const net = require("net");
+const {connect} = require('./client')
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
-    port: 50541 // PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  
-  conn.on("connect", () => {
-    console.log("Your data has been received from server");
-  });
-  
-  conn.on('data', (data) => {
-    console.log(data);
-  })
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
