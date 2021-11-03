494a4f2b392787099cbf584916908c978c5c2161
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2021-11-03 17:52:48 -0400

    Updated

--- play.js
+++ play.js
@@ -1,26 +1,7 @@
-// IP: 165.227.47.243 PORT: 50541 -- tells clients where to make the connection 
-// const net = require("net");
-const {connect} = require('./client')
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-// establishes a connection with the game server
 console.log("Connecting ...");
+connect();
 
-// setup interface to handle user input from stdin
-
-const handleUserInput = function (data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-handleUserInput(data);
 setupInput();
-
